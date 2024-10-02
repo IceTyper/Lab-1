7f7433fad0fe4383cdb6964d6f399a2b37ca21ce
icy <fmusaev217@gmail.com>
2024-10-02 23:02:55 +0300

    soxranis_uzor

--- uzor.py
+++ uzor.py
@@ -7,21 +7,19 @@ def draw_line(space, color):
     print(f'{" "*space}\x1b[48;5;255;1m{" "*color}\x1b[0m{" "*(space*2)}\x1b[48;5;255;1m{" "*color}\x1b[0m')
 
 
-width = 50
-space = (width-2)//2
-color = 1
-circler = 3
-while color < width:
+width = 54
+space = (width-4)//2
+color = 4
+circler = [16, 4, 8, 4, 4, 0, 4, 4, 0, 0, 0, 4, 0, 0]
+for i in range(len(circler)):
     draw_line(space, color)
-    space -= 2
-    color += 4
+    space -= circler[i]//2
+    color += circler[i]
 
-space, color = 1, width-3
-
-while color > 1:
+for i in range(len(circler)-1, -1, -1):
     draw_line(space, color)
-    space += 2
-    color -= 4
+    space += circler[i]//2
+    color -= circler[i]
 
 
 
